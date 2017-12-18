7272b6a8c4b3d2dcb857b4479b80aa9f0bb63340
Dewi Bryn Jones <d.b.jones@bangor.ac.uk>
2017-12-18 15:01:40 +0000

    llai o gof / less memory

--- marytts-languages/marytts-lang-cy/bin/mary.sh
+++ marytts-languages/marytts-lang-cy/bin/mary.sh
@@ -6,6 +6,6 @@ ps auxw | grep marytts-server | grep -v grep
 
 if [ $? != 0 ]
 then
-	bash /home/pi/src/marytts/target/marytts-$VERSION/bin/marytts-server -Xmx2g
+	bash /home/pi/src/marytts/target/marytts-$VERSION/bin/marytts-server -Xmx1g
 fi
 
